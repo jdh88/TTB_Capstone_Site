c200941579ce714ef2375d152201f5989d5b3867
Jonathan Hirokawa <johirokawa@gmail.com>
2017-09-26 21:54:56 -0400

    Switched to new single page theme, will need to remove junk later...

--- app.py
+++ app.py
@@ -1,34 +1,12 @@
 from flask import Flask, render_template, request, redirect
 from flask import flash
-from forms import TickerSymbolForm
-
-from stocks import get_dates, get_stock_data, plot_stocks
 
 app = Flask(__name__)
-app.config.from_object('config')
+#app.config.from_object('config')
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
-    form = TickerSymbolForm()
-    script = None
-    div = None
-    bad_result = None
-    if form.validate_on_submit():
-        company = form.ticker_symbol.data
-
-        #flash('You entered: %s' % (company))
-
-        dates = get_dates()
-
-        sdata = get_stock_data(company, dates[0], dates[1])
-        if sdata.empty:
-            bad_result = 'Data for {} not found'.format(company)
-        else:
-            script, div = plot_stocks(sdata, company)
-
-        #return redirect('/')
-    return render_template('index.html', form=form, bad_result=bad_result,
-                            script=script, div=div)
+    return render_template('index.html')
 
 @app.route('/about')
 def about():
--- forms.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, BooleanField
-from wtforms.validators import DataRequired
-
-class TickerSymbolForm(FlaskForm):
-    ticker_symbol = StringField('ticker_symbol', validators=[DataRequired()])
-    yesno = BooleanField('yesno', default=False)
